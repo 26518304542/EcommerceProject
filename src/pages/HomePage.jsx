c3a08d740d344b4e4d019d229f9ba956d0a3a46e
Özgür Işık Çimen <ozgurcimen1@gmail.com>
2025-11-16 00:20:12 +0300

    Fix: set Node version for Vercel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,10 +27,10 @@ export default function HomePage() {
   ];
 
   const products = [
-    { image: "/images/editorspic/fixed-height.png", title: "Beige Trench Coat", price: "89", rating: 4.6 },
-    { image: "/images/editorspic/fixed-height (1).png", title: "Casual Knit Sweater", price: "49", rating: 4.8 },
-    { image: "/images/editorspic/fixed-height (2).png", title: "Floral Dress", price: "69", rating: 4.5 },
-    { image: "/images/editorspic/fixed-height (3).png", title: "Chic Jumpsuit", price: "74", rating: 4.7 },
+    { image: "/images/bestsellers/fixed-height.png", title: "Beige Trench Coat", price: "89", rating: 4.6 },
+    { image: "/images/bestsellers/fixed-height (1).png", title: "Casual Knit Sweater", price: "49", rating: 4.8 },
+    { image: "/images/bestsellers/fixed-height (2).png", title: "Floral Dress", price: "69", rating: 4.5 },
+    { image: "/images/bestsellers/fixed-height (3).png", title: "Chic Jumpsuit", price: "74", rating: 4.7 },
     { image: "/images/bestsellers/fixed-height (4).png", title: "Light Top", price: "29", rating: 4.2 },
     { image: "/images/bestsellers/fixed-height (5).png", title: "Pleated Skirt", price: "39", rating: 4.4 },
     { image: "/images/bestsellers/fixed-height (6).png", title: "Pleated Skirt", price: "39", rating: 4.4 },
